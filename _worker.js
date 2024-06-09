de906f6ecd1efa39e636d81165037a5df3fba814
Albert的库 <133991461+Albert-s-library@users.noreply.github.com>
2024-06-09 22:23:19 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,14 +4,7 @@
 let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
-let addresses = [
-	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
-	'skk.moe#优选官方线路'
-	'icook.hk.443#优选官方线路'
-        'cf.090227.xyz#优选官方线路'
-        'cfip.xxxxxxxx.tk#优选官方线路'
-];
+let addresses = ['icook.tw:2053#官方优选域名','cloudflare.cfgo.cc#优选官方线路','skk.moe#优选官方线路','icook.hk.443#优选官方线路','cf.090227.xyz#优选官方线路','cfip.xxxxxxxx.tk#优选官方线路',];
 
 // 设置优选地址api接口
 let addressesapi = [
